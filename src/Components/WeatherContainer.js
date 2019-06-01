7e4f69d250e9cb75196fd6b4b188a085842cd04e
Ghevond97 <ghevond.mirzakhanyan@gmail.com>
2019-06-01 17:24:42 +0400

    api call created for fetching 5 day weather forecast for Yerevan

--- src/Components/WeatherContainer.js
+++ src/Components/WeatherContainer.js
@@ -1,7 +1,19 @@
 import React, {Component} from 'react'
-
+import apiKey from '../api/index.js'
 class WeatherContainer extends Component {
-  render(){
+
+  componentDidMount() {
+    this.getWeather();
+  }
+
+  getWeather = async () => {
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/forecast?q=Yerevan,am&appid=${apiKey}`);
+    const data = await api_call.json();
+    console.log(data);
+  }
+  render() {
     return(<div>testing</div>);
   }
 }
+
+export default WeatherContainer;
---
+++ src/api/index.js
@@ -0,0 +1,3 @@
+const apiKey = "7682661ef3352b3f86cf589cef1e777a";
+
+export default apiKey
