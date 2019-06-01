6777ae95691d2dc7981748d430ec1d1d66856326
Ghevond97 <ghevond.mirzakhanyan@gmail.com>
2019-06-01 21:40:44 +0400

    adding api call to gitIgnore, proceeding to cache clean up to ignore the api file in project in general

--- src/Components/WeatherContainer.js
+++ src/Components/WeatherContainer.js
@@ -1,13 +1,19 @@
 import React, {Component} from 'react'
 import apiKey from '../api/index.js'
 class WeatherContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      weatherData: null,
+    }
+  }
 
   componentDidMount() {
     this.getWeather();
   }
 
   getWeather = async () => {
-    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/forecast?q=Yerevan,am&appid=${apiKey}`);
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/forecast?q=Yerevan,am&appid=${apiKey}&units=metric`);
     const data = await api_call.json();
     console.log(data);
   }
