48baf517bdb26946dbc09dfe71b9ad6a630eb9e5
Ghevond97 <ghevond.mirzakhanyan@gmail.com>
2019-06-02 10:16:01 +0400

    fetch asyncs replaced with .thens, filterDailyWeather function added for filtering out the big json and giving us 5 days to work with, getDayOfWeek function created for converting dates to weekdays, minimal functionality added for rendering weather data.

--- src/Components/WeatherContainer.js
+++ src/Components/WeatherContainer.js
@@ -5,20 +5,64 @@ class WeatherContainer extends Component {
     super(props);
     this.state = {
       weatherData: null,
+      dailyData: null,
     }
   }
+ 
 
   componentDidMount() {
     this.getWeather();
   }
 
   getWeather = async () => {
-    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/forecast?q=Yerevan,am&appid=${apiKey}&units=metric`);
-    const data = await api_call.json();
-    console.log(data);
+    const url = `http://api.openweathermap.org/data/2.5/forecast?q=Yerevan,am&appid=${apiKey}&units=metric`;
+    fetch(url)
+    .then(res => res.json())
+    .then(data => {
+      const dailyData = this.filterDailyWeather(data);
+      this.setState({
+        fullData: data,
+        dailyData: dailyData
+      }, () => console.log(this.state))
+    })
   }
+  filterDailyWeather(data) {
+    console.log('filtering')
+    let dailyData;
+    if (data) {
+      console.log(data);
+    dailyData = data.list.filter(reading => {   
+        return reading.dt_txt.includes("18:00:00")
+        })
+    }
+  return dailyData  
+}
+  getDayOfWeek = (date) => {
+    var dayOfWeek = new Date(date).getDay();    
+    return isNaN(dayOfWeek) ? null : ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'][dayOfWeek];
+  }
+  
+  // alert( "Today is a " + getDayOfWeek(new Date()) + "!" );
+  
+
+  renderWeatherCards = () => {
+    const {dailyData} = this.state;
+    console.log('here');
+    return(<div>
+      {dailyData ? dailyData.map(data => {
+      return(
+      <div>
+        <div>{this.getDayOfWeek(data.dt_txt.slice(0,11))}</div>
+        <div>{data.main.temp_max}</div>
+        <div>{data.main.temp_min}</div>
+      </div>)
+    }): <div>not fetched</div>}
+    </div>);
+  }
+
+
   render() {
-    return(<div>testing</div>);
+    return(<>{this.renderWeatherCards()}</>)
   }
 }
 
